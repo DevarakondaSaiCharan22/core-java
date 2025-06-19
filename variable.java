2ba458fa5cb8fa9e6aec1dd4b3577e4cd0f87666
DevarakondaSaiCharan22 <DevarakondaSaiCharan22>
2025-06-19 16:45:00 +0530

    variable

---
+++ variable.java
@@ -0,0 +1,9 @@
+public class variable {
+    public static void main(String[] args) {
+        int age = 34;
+        String name = "sai";
+
+        System.out.println(age);
+        System.out.println(name);
+    }
+}
