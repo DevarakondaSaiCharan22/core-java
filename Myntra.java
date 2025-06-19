f2d5dbfac75a430a32ab49b6e8d64836e319c55a
DevarakondaSaiCharan22 <DevarakondaSaiCharan22>
2025-06-19 18:06:03 +0530

    Myntra & Mahindra datatypes

---
+++ Myntra.java
@@ -0,0 +1,23 @@
+public class Myntra {
+    
+     public static void main(String[] args) {
+
+    String brand = "HIGHLANDER";
+    String description = "Men Comfort Relaxed Fit Textured Spread Collar Casual Shirt";
+    float rating = 4.6f;
+    int price = 50;
+    float discount = 21.0f;
+    int maxprice = 1500;
+    int sellprice = 750;
+    byte size = 44;
+
+    System.out.println(brand);
+    System.out.println(description);
+    System.out.print(rating);
+    System.out.print(price);
+    System.out.println(discount);
+    System.out.println(maxprice);
+    System.out.println(sellprice);
+    System.out.println(size);
+  }
+}
