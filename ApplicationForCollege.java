034829b222b0fb61228da128c4635984b78b960d
DevarakondaSaiCharan22 <DevarakondaSaiCharan22>
2025-06-20 15:19:13 +0530

    Application for college

---
+++ ApplicationForCollege.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class ApplicationForCollege {
+    
+    public static void main(String[] args) {
+        
+        String lastName = "Devarakonda";
+        String firestName = "Sai charan";
+        int modileNum = 994242424;
+        String address = "KNR, TELANGANA";
+        int DateOfBirth = 22-12-2002;
+        String birthPlace = "KNR";
+        String civilStatus = "Single";
+        char sex = 'M';
+        String f_NAme = "Manohar";
+        String f_occupation = "Gold-smith";
+        int f_Num = 823242342;
+        String f_Addres = "KNR";
+
+        String highSchool = "St.marys High school";
+        short year = 2018;
+
+        System.out.println("Last Name  :" + lastName );
+         System.out.println("First Name  :" + firestName );
+         System.out.println("Modile number  :" + modileNum );
+         System.out.println("Address  :" + address );
+         System.out.println("Date of Birth  :" + DateOfBirth );
+         System.out.println("Birth Place  :" + birthPlace );
+         System.out.println("Civil Status  :" + civilStatus );
+         System.out.println("Sex  :" + sex );
+         System.out.println("Father Name  :" + f_NAme );
+         System.out.println("Father Location  :" + f_occupation );
+         System.out.println("Father Number  :" + f_Num );
+         System.out.println("Father Address :" + f_Addres );
+         System.out.println("School Name  :" + highSchool );
+         System.out.println("Batch  :" + year );
+
+    }
+}
